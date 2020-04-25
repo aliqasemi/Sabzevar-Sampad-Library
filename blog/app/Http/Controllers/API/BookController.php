e1f7b8ad438d6eb8f4524a90f6e10f575d1b2e97
ali qasemi <aliqasemi4245qq@gmail.com>
2020-04-26 03:15:20 +0430

    make API bookController and make index of json!

---
+++ blog/app/Http/Controllers/API/BookController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\book;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        //
+        $book = book::paginate(2) ;
+        if(!$book){
+            return response()->json([
+                'message' => 'not found book!' ,
+
+            ] , 404) ;
+        }
+        else
+            return response()->json($book , 200) ;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    /*public function create()
+    {
+        //
+    }*/
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    /*public function edit($id)
+    {
+        //
+    }*/
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function destroy($id)
+    {
+        //
+    }
+}
